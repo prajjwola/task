466ba671cd8db46409a1a81d5b4c11ed2e77f2bd
prajjwola <167495848+prajjwola@users.noreply.github.com>
2024-05-09 15:17:10 +0545

    Update operators.js

---
+++ javascript/basic/operators.js
@@ -0,0 +1,32 @@
+const arr = [3,5,2];
+const backupArr = [...arr];
+
+//Q1 calculate length of array and console log
+//expected output is 3
+console.log(backupArr.length);
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+let reverse = backupArr.reverse();
+console.log(backupArr);
+
+///Q3 join the array and console log
+// expected output 352
+console.log(arr.join(''));
+
+//Q4 Remove the last element from the array and log the modified array to the console
+const secondBackUpArr = [...arr];
+secondBackUpArr.pop();
+console.log(secondBackUpArr);
+
+// // Q5 Check if all elements in the num are even. Log "All even" or "Not all even" accordingly.
+const num = [6,4,2,8,10,12]
+
+let allEven = num.every(num => num%2===0);
+console.log(allEven);
+if(allEven){
+    console.log("All Even");
+}else{
+    console.log("Not All Even");
+}
+
